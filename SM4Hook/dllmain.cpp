45455ea7492b724877f17c4b8be43b5ddce8bb88
GEEKiDoS <geek_ds@foxmail.com>
2019-12-25 12:58:01 +0800

    Update for SM4.3.3

--- SM4Hook/dllmain.cpp
+++ SM4Hook/dllmain.cpp
@@ -1,11 +1,14 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
 
-#define VERSION "0.1.1"
-#define TARGETVERSION "4.2.3"
+#define VERSION "0.1.2"
+#define TARGETVERSION "4.3.3"
 
 #ifdef _WIN64
 typedef unsigned long long ADDR_LEN;
+#else
+typedef unsigned long ADDR_LEN;
+#endif
 
 template<size_t iDataSize>
 static void WriteProtectedMemory(uintptr_t pDestination, const std::array<uint8_t, iDataSize>& data)
@@ -18,10 +21,6 @@ static void WriteProtectedMemory(uintptr_t pDestination, const std::array<uint8_
 		FlushInstructionCache(GetCurrentProcess(), (LPCVOID)pDestination, iDataSize);
 	}
 }
-
-#else
-typedef unsigned long ADDR_LEN;
-#endif
 
 void Sys_RunInit()
 {
@@ -41,16 +40,12 @@ void Sys_RunInit()
 		ExitProcess(-1);
 	}
 #else
-	if (*(DWORD*)(hModule + 0x87124) == 0x24448948) //SM4.2.3 x64!
+	if (*(BYTE*)(hModule + 0x885D0) == 0x0F) //SM4.3.3 x64!
 	{
-		const std::array<uint8_t, 1> jzToJnZ = { 0x75 };
-
-		WriteProtectedMemory((uintptr_t)(hModule + 0x87146), jzToJnZ);
-
-		if (_access("_edu_", 0) != -1)
-			WriteProtectedMemory((uintptr_t)(hModule + 0x87184), jzToJnZ);
-		else
-			WriteProtectedMemory((uintptr_t)(hModule + 0x8715D), jzToJnZ);
+		const std::array<uint8_t, 4> patch = { 0xE9, 0x4D, 0x08, 0x00 };
+		const std::array<uint8_t, 1> patch1 = { 0x01 };
+		WriteProtectedMemory((uintptr_t)(hModule + 0x885D0), patch);
+		WriteProtectedMemory((uintptr_t)(hModule + 0x88E2F), patch1);
 	}
 	else
 	{
@@ -60,77 +55,10 @@ void Sys_RunInit()
 #endif
 }
 
-#ifndef _WIN64
-static BYTE originalCode[5];
-static PBYTE originalEP = 0;
-
-void Main_UnprotectModule(HMODULE hModule)
-{
-	PIMAGE_DOS_HEADER header = (PIMAGE_DOS_HEADER)hModule;
-	PIMAGE_NT_HEADERS ntHeader = (PIMAGE_NT_HEADERS)((ADDR_LEN)hModule + header->e_lfanew);
-
-	// unprotect the entire PE image
-	SIZE_T size = ntHeader->OptionalHeader.SizeOfImage;
-	DWORD oldProtect;
-	VirtualProtect((LPVOID)hModule, size, PAGE_EXECUTE_READWRITE, &oldProtect);
-}
-
-void Main_DoInit()
-{
-	// return to the original EP
-	memcpy(originalEP, &originalCode, sizeof(originalCode));
-
-	// unprotect our entire PE image
-	HMODULE hModule;
-	if (SUCCEEDED(GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCSTR)Main_DoInit, &hModule)))
-	{
-		Main_UnprotectModule(hModule);
-	}
-
-	Sys_RunInit();
-
-	hModule = GetModuleHandle(NULL);
-	PIMAGE_DOS_HEADER header = (PIMAGE_DOS_HEADER)hModule;
-	PIMAGE_NT_HEADERS ntHeader = (PIMAGE_NT_HEADERS)((ADDR_LEN)hModule + header->e_lfanew);
-
-	// back up original code
-	originalEP = (PBYTE)((ADDR_LEN)hModule + ntHeader->OptionalHeader.AddressOfEntryPoint);
-
-	__asm jmp originalEP
-}
-
-void Main_SetSafeInit()
-{
-	// find the entry point for the executable process, set page access, and replace the EP
-	HMODULE hModule = GetModuleHandle(NULL); // passing NULL should be safe even with the loader lock being held (according to ReactOS ldr.c)
-
-	if (hModule)
-	{
-		PIMAGE_DOS_HEADER header = (PIMAGE_DOS_HEADER)hModule;
-		PIMAGE_NT_HEADERS ntHeader = (PIMAGE_NT_HEADERS)((ADDR_LEN)hModule + header->e_lfanew);
-
-		Main_UnprotectModule(hModule);
-
-		// back up original code
-		PBYTE ep = (PBYTE)((ADDR_LEN)hModule + ntHeader->OptionalHeader.AddressOfEntryPoint);
-		memcpy(originalCode, ep, sizeof(originalCode));
-
-		// patch to call our EP
-		ADDR_LEN newEP = (ADDR_LEN)Main_DoInit - ((ADDR_LEN)ep + 5);
-		ep[0] = 0xE9; // for some reason this doesn't work properly when run under the debugger
-		memcpy(&ep[1], &newEP, 4);
-
-		originalEP = ep;
-	}
-}
-#endif
 bool __stdcall DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-#ifndef _WIN64
-		Main_SetSafeInit();
-#else
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
 		Sys_RunInit();
-#endif
 	}
 
 	return true;
